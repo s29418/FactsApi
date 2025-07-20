d60882b0a401fce1ca136a936ca9b9d0353116da
s29418 <s29418@pjwstk.edu.pl>
2025-07-20 11:15:54 +0200

    Added FactService with HttpClient and DI

---
+++ Models/Fact.cs
@@ -0,0 +1,7 @@
+﻿namespace FactsApi.Models;
+
+public class Fact
+{
+    public string FactText { get; set; }
+    public int Length { get; set; }
+}
---
+++ Services/IFactService.cs
@@ -0,0 +1,8 @@
+﻿using FactsApi.Models;
+
+namespace FactsApi.Services;
+
+public interface IFactService
+{
+    Task<Fact> GetFactAsync();
+}
